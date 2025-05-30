20941d6573746d9e2f8e0619ac690ce5132f5ed0
YRadwan <43502481+YusRadwan@users.noreply.github.com>
2025-05-30 19:10:55 +0300

    delete

--- 6- Project/1- Online Shop/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-const elemLoggin = document.getElementById('loggin');
-if(window.location.pathname !== '/') {
-    console.log(`path is -> ${window.location.pathname}`);
-    elemLoggin.setAttribute('class', 'active');
-}
